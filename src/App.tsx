84a7a4f0f16f3ce752be15cd0fa366a35af549eb
Krystian Sledz <krystiansledz419@gmail.com>
2022-05-09 23:36:59 +0200

    Login view

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import RouterSwitch from "./router";
 import Providers from "./providers";
+import { Container } from "@mui/material";
 
 function App() {
   return (
     <Providers>
-      <RouterSwitch />
+      <Container
+        sx={{
+          display: "flex",
+          minHeight: "100vh",
+          width: "100vw",
+          overflow: "hidden",
+        }}
+      >
+        <RouterSwitch />
+      </Container>
     </Providers>
   );
 }
